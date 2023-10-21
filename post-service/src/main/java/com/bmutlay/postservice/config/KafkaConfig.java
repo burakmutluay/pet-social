4487a4027772e7d22ac4573faf4b5bb39d9c1cc7
bmutlay <mburakmutluay@gmail.com>
2023-10-21 21:12:53 +0300

    admin config issue resolved

--- post-service/src/main/java/com/bmutlay/postservice/config/KafkaConfig.java
+++ post-service/src/main/java/com/bmutlay/postservice/config/KafkaConfig.java
@@ -1,13 +1,24 @@
 package com.bmutlay.postservice.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
 
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "kafkaserver:9092");
+        return new KafkaAdmin(props);
+    }
     @Bean
     public NewTopic transactionTopic() {
         return TopicBuilder.name("post-topic")
