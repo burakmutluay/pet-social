7b2bc68c7226dd5d5a25f6881999155a36189a36
bmutlay <mburakmutluay@gmail.com>
2023-10-22 18:54:26 +0300

    findLatestPosts query method added

--- post-service/src/main/java/com/bmutlay/postservice/repository/PostRepository.java
+++ post-service/src/main/java/com/bmutlay/postservice/repository/PostRepository.java
@@ -3,8 +3,8 @@ package com.bmutlay.postservice.repository;
 import com.bmutlay.postservice.model.Post;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
@@ -12,5 +12,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByUsernameOrderByCreatedAtDesc(String username);
 
     List<Post> findAllByUsername(String username, Pageable pageable);
+
+    List<Post> findPostsByCreatedAtAfterAndUsername(Instant instant, String username);
 
 }
